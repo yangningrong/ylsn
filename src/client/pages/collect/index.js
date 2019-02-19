3ee068c5dcaad2ed26b6d833317d6f3eeb5305b9
5se7en <177739007@qq.com>
2019-02-19 22:48:00 +0800

    修改webpack配置

--- src/client/pages/collect/index.js
+++ src/client/pages/collect/index.js
@@ -24,15 +24,7 @@ export default class Collect extends Component {
   render() {
     return (
       <div className="collect-page">
-        {/* <div className="collect-header">
-          添加合约商品
-        </div> */}
         <Header>添加合约商品</Header>
-        {/* <div className="collect-body">
-          <span>空间类型<input type="text" className="name" placeholder="选择空间类型"></input><br /></span>
-          <span>空间内容<input type="text" className="old" placeholder="请输入空间名称"></input><br /></span>
-
-        </div> */}
       </div>
     );
   }
